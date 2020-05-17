35e6a4290228236240c43dbcbb4831749632c887
coder <anthony.john.fawcett@gmail.com>
2020-05-17 22:24:11 +1000

    Add fonts, styling, and animation

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({item, subtitle}) => 
-<div className={`menu-item ${item.size ||''}`} style={{
-    backgroundImage: `url(${process.env.PUBLIC_URL}/media/images/${item.title}/section.png)`
-}}>
+<div className={`menu-item ${item.size ||''}`}>
+    <div className="background-image" style={{
+        backgroundImage: `url(${process.env.PUBLIC_URL}/media/images/${item.title}/section.png)`
+    }}>
+    </div>
     <div className='content'>
         <h1 className='title'>{item.title}</h1>
         <span className='subtitle'>{subtitle}</span>
