7d8970adf86b138f19e86b64a77ad41d867ec795
Anthony Fawcett <anthonyjohnfawcett@gmail.com>
2020-05-17 20:17:23 +1000

    Updating github pages

---
+++ precache-manifest.dc3f0c77891441d11a611567d650387c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f745b03e1d7ac8561935d19127b9b481",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c33a7850bfe191e64bf2",
+    "url": "/static/css/main.ae8afe1a.chunk.css"
+  },
+  {
+    "revision": "b40a43c44c681f4a37a9",
+    "url": "/static/js/2.7383570d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7383570d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33a7850bfe191e64bf2",
+    "url": "/static/js/main.e934b137.chunk.js"
+  },
+  {
+    "revision": "bb6ad3df7cd9370dc4be",
+    "url": "/static/js/runtime-main.a4730ddd.js"
+  }
+]);
